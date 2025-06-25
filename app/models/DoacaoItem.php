45c01f26abfb24ae91b40e325892b002f4b97bbe
paulornr89 <paulornr89@gmail.com>
2025-06-25 20:01:46 -0300

    inclusão da classe doacao item

---
+++ app/models/DoacaoItem.php
@@ -0,0 +1,25 @@
+<?php
+    class DoacaoItem {
+        private $id_item;
+        private $id_doacao;
+        private $quantidade;
+
+        public function __construct($id_item, $id_doacao, $quantidade) {
+            $this->id_item = $id_item;
+            $this->$id_doacao = $id_doacao;
+            $this->quantidade = $quantidade;            
+        }
+
+        public function getIdItem() {
+            return $this->id_item;
+        }
+
+        public function getIdDoacao() {
+            return $this->id_doacao;
+        }
+
+        public function getQuantidade() {
+            return $this->quantidade;
+        }
+    }
+?>
